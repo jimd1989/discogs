e74deb4210a26fd74ba94b7d1249211d5b78d428
jimd1989 <jamesdalrymple@protonmail.com>
2020-07-23 15:18:30 +1000

    Parameterization

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,30 @@
 module Main where
 
+import Data.Functor (($>))
+import Data.List (find)
+import Data.Maybe (isJust)
+import Data.Tuple (uncurry)
 import System.Environment (getArgs)
+import System.Process (system)
 import Fetching (fetch)
 import Parsing (decode')
 import Processing (commands)
 
-release ∷ [Char]
---release = "https://www.discogs.com/Akiko-Yano-%E3%81%9F%E3%81%A0%E3%81%84%E3%81%BE/release/12652701"
-release = "https://www.discogs.com/%E7%9F%A2%E9%87%8E%E9%A1%95%E5%AD%90-%E3%81%9F%E3%81%A0%E3%81%84%E3%81%BE/release/1852193"
+runCmds ∷ [[Char]] → [[Char]] → IO ()
+runCmds α ω = mapM_ (uncurry run) (zip α ω)
+  where run α ω = system (α <> " " <> ω) $> ()
 
 main :: IO ()
 main = do
-  album ← decode' <$> fetch release
-  cmds  ← pure $ (commands "Pop") <$> album
-  _ ← case cmds of
+  args     ← getArgs
+  absolute ← pure $ isJust $ find ((== "-a")) args
+  rest     ← pure $ filter (/= "-a") args
+  genre    ← pure $ rest !! 0
+  url      ← pure $ rest !! 1
+  files    ← pure $ tail $ tail rest
+  album    ← decode' <$> fetch url
+  cmds     ← pure $ (commands genre) <$> album
+  _        ← case cmds of
     Nothing → pure ()
     Just α  → mapM_ putStrLn α
   return ()
