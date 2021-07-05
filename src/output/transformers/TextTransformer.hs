510fcb309590cd89fa827bbce08ba57a45836301
jimd1989 <jamesdalrymple@protonmail.com>
2021-07-05 18:31:52 +1000

    Text placeholders, replacement for string ops

--- src/output/transformers/TextTransformer.hs
+++ src/output/transformers/TextTransformer.hs
@@ -7,6 +7,7 @@ import Data.Char (toLower)
 import Data.List (drop, init, intercalate, take)
 import Data.List.Split (splitOn)
 import Data.Set (Set, fromList, member)
+import qualified Data.Text as T
 import Helpers ((◁), (◇), (⊙), fork, last')
 
 lower ∷ Set String
@@ -16,12 +17,6 @@ lower = fromList ["A", "An", "And", "By", "In", "On", "Of", "At", "With", "The",
 checkCaps ∷ String → String
 checkCaps α = if member α lower then (toLower ⊙ α) else α
 
-onLast ∷ (a → [a]) → [a] → Maybe [a]
-onLast f α = ((init α ◇) . f) ⊙ (last' α)
-
-onTail ∷ (a → a) → [a] → [a]
-onTail f = fork (◇) (take 1) (f ◁ drop 1)
-
 onWords ∷ ([String] → a) → String → a
 onWords f = f . splitOn " "
 
@@ -30,3 +25,26 @@ fromWords = intercalate " "
 
 transformText ∷ String → String
 transformText = fromWords . onWords (onTail checkCaps)
+
+-- new
+lower' ∷ Set T.Text
+lower' = fromList ["A", "An", "And", "By", "In", "On", "Of", "At", "With", "The",
+                  "For", "From", "Into", "Unto", "To", "As"]
+
+checkCaps' ∷ T.Text → T.Text
+checkCaps' α = if member α lower' then T.toLower α else α
+
+onLast ∷ (a → [a]) → [a] → Maybe [a]
+onLast f α = ((init α ◇) . f) ⊙ (last' α)
+
+onTail ∷ (a → a) → [a] → [a]
+onTail f = fork (◇) (take 1) (f ◁ drop 1)
+
+onWords' ∷ ([T.Text] → a) → T.Text → a
+onWords' f = f . T.splitOn " "
+
+fromWords' ∷ [T.Text] → T.Text
+fromWords' = T.intercalate " "
+
+transformText' ∷ T.Text → T.Text
+transformText' = fromWords' . onWords' (onTail checkCaps')
